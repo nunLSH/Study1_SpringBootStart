13572105f731aeb2a601756f0258a7b3658d3244
Seohyeon.Lim <forestlight749@gmail.com>
2024-01-17 22:11:38 +0900

    (수정) 스프링 데이터 JPA를 사용하여 DB와 연결

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -12,23 +12,24 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
-
-    @Bean // 스프링 빈을 등록할 거라는 의미
-    public MemberService memberService() { // memberService가 스프링 빈에 등록됨
-        return new MemberService(memberRepository());
-        // 스프링 빈에 등록된 memberRepository를 MemberService에 넣어줌
+    @Autowired  // 생성자가 하나인 경우에는 생략 가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository() { // memberRepository가 스프링 빈에 등록됨
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() { // memberRepository가 스프링 빈에 등록됨
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
